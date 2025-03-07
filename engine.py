a75226959bf901f09852745a43b30def208a7b35
Andrew Xie <70659392+ax1313@users.noreply.github.com>
2025-03-06 22:22:23 -0500

    Add files via upload

--- engine.py
+++ engine.py
@@ -2,7 +2,6 @@ ticker_list = []
 buy_list = []
 sell_list = []
 
-# Add wrapper functionality after completing this function
 def addOrder(order_type, ticker_symbol, quantity, price):
     if len(ticker_list) < 1024:
         if ticker_symbol not in ticker_list:
@@ -13,17 +12,20 @@ def addOrder(order_type, ticker_symbol, quantity, price):
         elif order_type.lower() == "sell":
             sell_list.insert(0, [ticker_symbol, quantity, price])
 
+
 # Return matching sell order
 def matchOrder(ticker_symbol):
 
     curr_buys = []
     curr_sells = []
 
-    # Check compatability
+    # Check compatibility before matching
     max_buy_price = 0
+    sum_buy_quantity = 0
     for buy in buy_list:
         if buy[0] == ticker_symbol:
             curr_buys.insert(0, [buy[1], buy[2]])
+            sum_buy_quantity += buy[1]
             if buy[2] > max_buy_price:
                 max_buy_price = buy[2]
 
@@ -35,6 +37,14 @@ def matchOrder(ticker_symbol):
                 min_sell_price = sell[2]
     
     if max_buy_price >= min_sell_price:
-        
-
-    return 0
+        sum_sell_quantity = 0
+        sell_ret_list = []
+        for sell in curr_sells:
+            if sum_sell_quantity < sum_buy_quantity:
+                sum_sell_quantity += sell[1]
+                sell_ret_list.append(sell)
+            else:
+                break
+        return sell_ret_list
+    else:
+        return 0
